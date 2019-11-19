30d138b463729929898fd87d1afb5dcd83b2850a
Sairam Ghanta <sairam.ghanta@wirelesscar.com>
2019-11-19 10:12:09 +0100

    seperate authprovider

--- src/main/java/com/ram/sample/security/UserRepository.java
+++ src/main/java/com/ram/sample/security/UserRepository.java
@@ -2,6 +2,8 @@ package com.ram.sample.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ram.sample.model.User;
+
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
